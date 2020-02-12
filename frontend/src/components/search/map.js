f36a85f018234f06c1dbc3fe87c6ce7e13897983
Kieran Scannell <kieran.t.scannell@gmail.com>
2020-02-12 09:30:09 -0600

    Add ability to place marker on map

--- frontend/src/components/search/map.js
+++ frontend/src/components/search/map.js
@@ -11,23 +11,40 @@ const mapOptions = {
   zoom: 13
 }; 
 
+// this is an example spots array for testing marker manager
+const spots = [{ id: 1, lat: 37.773972, lng: -122.431297 }]
+// testing end
+
 export default class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.apiIsLoaded = this.apiIsLoaded.bind(this);
+  }
   componentDidMount() {
-    this.MarkerManager = new MarkerManager(this.map);
-    this.MarkerManager.updateMarkers();
+    // this.MarkerManager = new MarkerManager(this.map);
+    // this.MarkerManager.updateMarkers();
   }
 
   componentDidUpdate() {
-    this.MarkerManager.updateMarkers();
+    this.MarkerManager.updateMarkers(spots);
   }
+
+  apiIsLoaded(map, maps) {
+    this.MarkerManager = new MarkerManager(map, maps);
+    this.MarkerManager.updateMarkers(spots);
+  }
+
   render() {
-    debugger;
     return (
       <div id='map-container'>
         <GoogleMapReact
           bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_API_KEY }}
           defaultZoom={mapOptions.zoom}
           defaultCenter={mapOptions.center}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => (
+            this.apiIsLoaded(map, maps)
+          )}
         />
       </div>
     )
--- frontend/src/util/marker_manager.js
+++ frontend/src/util/marker_manager.js
@@ -1,10 +1,41 @@
 export default class MarkerManager {
-  constructor(map) {
+  constructor(map, maps) {
     this.map = map;
+    this.maps = maps;
     this.markers = {};
   }
   
-  updateMarkers(benches) {
-    console.log("time to update");
+  updateMarkers(spots) {
+    const spotsObj = {};
+    spots.forEach(spot => spotsObj[spot.id] = spot);
+
+    // Create markers for all the passed in spots that don't yet have markers
+    spots
+      .filter(spot => !this.markers[spot.id])
+      .forEach(newspot => this.createMarkerFromSpot(newspot, this.handleClick))
+
+    // Remove markers for any spots that were not passed in
+    Object.keys(this.markers)
+      // filter out markers for spots that were passed in 
+      .filter(spotId => !spotsObj[spotId])
+      // Remove markers for remaining spots (those that were not passed in)
+      .forEach(spotId => this.removeMarker(this.markers[spotId]))
+  }
+
+  createMarkerFromSpot(spot) {
+    const position = new this.maps.LatLng(spot.lat, spot.lng);
+    const marker = new this.maps.Marker({
+      position,
+      map: this.map,
+      spotId: spot.id
+    });
+
+    marker.addListener('click', () => this.handleClick(spot));
+    this.markers[marker.spotId] = marker;
+  }
+
+  removeMarker(marker) {
+    this.markers[marker.spotId].setMap(null);
+    delete this.markers[marker.spotId];
   }
 }
