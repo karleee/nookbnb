8b6cf73208d07b2412a868a128ecefc820d85822
Kieran Scannell <kieran.t.scannell@gmail.com>
2020-02-12 08:27:43 -0600

    Add marker manager utility

--- frontend/src/components/search/map.js
+++ frontend/src/components/search/map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './search.css';
 import GoogleMapReact from 'google-map-react';
+import MarkerManager from '../../util/marker_manager';
 
 const mapOptions = {
   center: {
@@ -12,7 +13,12 @@ const mapOptions = {
 
 export default class Map extends Component {
   componentDidMount() {
-  
+    this.MarkerManager = new MarkerManager(this.map);
+    this.MarkerManager.updateMarkers();
+  }
+
+  componentDidUpdate() {
+    this.MarkerManager.updateMarkers();
   }
   render() {
     debugger;
