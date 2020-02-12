6c21000fe4a19f82f2c7455caf82809ef490c854
Kieran Scannell <kieran.t.scannell@gmail.com>
2020-02-12 06:31:33 -0600

    Add search and map components

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,6 +4,7 @@ import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
+import Search from "./search/search";
 
 const App = () => (
 	<div>
@@ -12,6 +13,7 @@ const App = () => (
 			{/* <AuthRoute exact path="/" component={} /> */}
 			<AuthRoute exact path="/login" component={LoginFormContainer} />
 			<AuthRoute exact path="/signup" component={SignupFormContainer} />
+			<ProtectedRoute path="/search" component={Search} />
 		</Switch> 
     <h1>hello world</h1>
 	</div>
---
+++ frontend/src/components/search/map.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './search.css';
+
+export default class Map extends Component {
+  render() {
+    return (
+      <div id='map-container' ref='map'>
+      </div>
+    )
+  }
+}
