c0a7c28eabcbba353337f9325e850a95d6851ce6
Kieran Scannell <kieran.t.scannell@gmail.com>
2020-02-12 13:15:04 -0600

    Add actions and update reducer to receive filtered spots

--- frontend/src/actions/spot_actions.js
+++ frontend/src/actions/spot_actions.js
@@ -1,14 +1,29 @@
-import { getSpots } from '../util/spots_api_util';
+import { 
+  getSpots,
+  getFilteredSpots 
+} from '../util/spots_api_util';
 
 export const RECEIVE_ALL_SPOTS = 'RECEIVE_ALL_SPOTS';
+export const RECEIVE_SPOTS = 'RECEIVE_SPOTS';
 
 export const receiveAllSpots = spots => ({
   type: RECEIVE_ALL_SPOTS,
   spots
 });
 
+export const receiveSpots = spots => ({
+  type: RECEIVE_SPOTS,
+  spots
+});
+
 export const fetchSpots = () => dispatch => ( 
   getSpots()
     .then(spots => dispatch(receiveAllSpots(spots)))
+    .catch(err => console.log(err))
+);
+
+export const fetchFilteredSpots = bounds => dispatch => (
+  getFilteredSpots(bounds)
+    .then(spots => dispatch(receiveSpots(spots)))
     .catch(err => console.log(err))
 );
--- frontend/src/reducers/spots_reducer.js
+++ frontend/src/reducers/spots_reducer.js
@@ -1,10 +1,14 @@
-import { RECEIVE_ALL_SPOTS } from '../actions/spot_actions';
+import { 
+  RECEIVE_ALL_SPOTS,
+  RECEIVE_SPOTS 
+} from '../actions/spot_actions';
 
 export default function(state = {}, action) {
   Object.freeze(state);
 
   switch (action.type) {
     case RECEIVE_ALL_SPOTS:
+    case RECEIVE_SPOTS:
       return Object.assign({}, state, action.spots.data);
     default:
       return state;
