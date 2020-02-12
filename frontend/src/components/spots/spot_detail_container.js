ba3e5070d4c8b2d1899329d1dc21983a036a001c
karleee <karleee@protonmail.com>
2020-02-12 09:19:57 -0800

    Add get spot route for backend

--- frontend/src/components/spots/spot_detail_container.js
+++ frontend/src/components/spots/spot_detail_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import SpotDetail from './spot_detail';
 
+// Mapping values of spotId to props
 const mapStateToProps = (state, ownProps) => ({
   spotId: ownProps.match.params.spotId,
 });
--- frontend/src/util/spots_api_util.js
+++ frontend/src/util/spots_api_util.js
@@ -1,5 +1,11 @@
 import axios from 'axios';
 
+// Backend route for retrieving all spots
 export const getSpots = () => {
   return axios.get('/api/spots');
+};
+
+// Backend route for retrieving a specific spot
+export const getSpot = id => {
+  return axios.get('/api/spots/:spotId');
 };
