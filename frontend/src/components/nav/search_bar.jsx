36bfa66b6590c830fda0150118d183041de477f4
karleee <karleee@protonmail.com>
2020-02-16 21:00:24 -0800

    Changed nav bar to fixed position

--- frontend/src/components/nav/search_bar.jsx
+++ frontend/src/components/nav/search_bar.jsx
@@ -18,7 +18,7 @@ class SearchBar extends React.Component {
 	}
 
 	toggleSearchPlaceholder() {
-	  this.setState({searchPlaceholder: !this.state.searchPlaceholder});
+		this.setState({searchPlaceholder: !this.state.searchPlaceholder});
 	}
 
 	handleUpdate() {
