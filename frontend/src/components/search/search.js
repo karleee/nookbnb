00cad56ef761ce51b117eb5cb6bc840fd21861b6
Kieran Scannell <kierscan@gmail.com>
2020-02-18 09:34:05 -0600

    Add css class to container element

--- frontend/src/components/search/search.js
+++ frontend/src/components/search/search.js
@@ -3,7 +3,7 @@ import Map from './map';
 
 export default function Search({ spots, requestUpdateBounds, geocode, center }) {
   return (
-    <div>
+    <div className="map-container">
       <Map 
         requestUpdateBounds={requestUpdateBounds}
         geocode={geocode}
