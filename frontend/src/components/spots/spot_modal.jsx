cb3a8c3fd4a2c10c1c58d43c1098ba9c39d08c68
karleee <karleee@protonmail.com>
2020-02-21 13:50:05 -0800

    Add styling to spot modal

--- frontend/src/components/spots/spot_modal.jsx
+++ frontend/src/components/spots/spot_modal.jsx
@@ -5,32 +5,36 @@ import '../../assets/stylesheets/spot_modal.css';
 class SpotModal extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentImage: this.props.modal.imageNum
+    }
   }
 
   render() {
-    const { modal, spot } = this.props;
-    let imageNum = modal.imageNum;
+    const { spot } = this.props;
 
     return (
       <div className="spot-modal">
         <div className="selected-image-wrapper">
-          <img src={spot.thumbnail_image_urls ? spot.thumbnail_image_urls[imageNum] : ''} alt="Spot photo" />
+          <img src={spot.thumbnail_image_urls ? spot.thumbnail_image_urls[this.state.currentImage] : ''} alt="Spot photo" />
         </div> 
 
-        <div className="close-wrapper" onClick={this.props.closeModal}>
-          <img src='/images/spot_modal/modal_close.png' />
-        </div>
+        <div className="side-images-wrapper">
+          <div className="close-wrapper" onClick={this.props.closeModal}>
+            <img src='/images/spot_modal/modal_close.png' />
+          </div>
 
-        {/* <div className="side-images-wrapper">
-          <div className="side-images">
-            {spot.thumbnail_image_urls ? spot.thumbnail_image_urls.slice(imageNum + 1, spot.thumbnail_image_urls.length).map(url =>
-              <div className="image">
+          <div className="thumbnails">
+            {spot.thumbnail_image_urls ? spot.thumbnail_image_urls.map((url, indx) =>
+              <div className={`thumbnail-wrapper ${indx === this.state.currentImage ? "active" : ""}`}>
                 <img src={url} alt="Spot photo" />
               </div>) : ''}
           </div>
 
-          <p>{imageNum + 1} / {spot.thumbnail_image_urls.length}</p>
-        </div> */}
+          <div className="numbering">
+            <p>{this.state.currentImage + 1} / {spot.thumbnail_image_urls.length}</p>
+          </div>
+        </div>
       </div>
     );
   }
