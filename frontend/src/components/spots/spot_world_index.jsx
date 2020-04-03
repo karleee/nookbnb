d9acbde1cb400431461ea3cb180d846940162137
karleee <karleee@protonmail.com>
2020-04-03 16:11:52 -0700

    Fix world regions bar

--- frontend/src/components/spots/spot_world_index.jsx
+++ frontend/src/components/spots/spot_world_index.jsx
@@ -13,28 +13,28 @@ class SpotWorldIndex extends React.Component {
       <div className="world-container"> 
         <div className="breezy-hollow-wrapper">
           <div className="image-wrapper">
-            {/* <img src='/images/homepage/breezy_hollow.png' /> */}
+            <img src='/images/homepage/breezy_hollow.png' />
           </div>
           <p>Breezy Hollow</p>
         </div>
 
         <div className="lost-lure-creek-wrapper">
           <div className="image-wrapper">
-            {/* <img src='/images/homepage/lost_lure_creek.png' /> */}
+            <img src='/images/homepage/lost_lure_creek.png' />
           </div>
           <p>Lost Lure Creek</p>
         </div>
 
         <div className="saltwater-shores-wrapper">
           <div className="image-wrapper">
-            {/* <img src='/images/homepage/saltwater_shores.png' /> */}
+            <img src='/images/homepage/saltwater_shores.png' />
           </div>
           <p>Saltwater Shores</p>
         </div>
 
         <div className="sunburst-island-wrapper">
           <div className="image-wrapper">
-            {/* <img src='/images/homepage/sunburst_island.png' /> */}
+            <img src='/images/homepage/sunburst_island.png' />
           </div>
           <p>Sunburst Island</p>
         </div>
