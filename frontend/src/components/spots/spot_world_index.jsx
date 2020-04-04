d20122768d9764ac7010ba1e51c605fd12ed8f71
karleee <karleee@protonmail.com>
2020-04-03 19:07:43 -0700

    Fix image sizing

--- frontend/src/components/spots/spot_world_index.jsx
+++ frontend/src/components/spots/spot_world_index.jsx
@@ -7,33 +7,33 @@ class SpotWorldIndex extends React.Component {
   }
 
   render() {
-    const { spots } = this.props;
+    const { spots } = this.props; 
 
     return (
       <div className="world-container"> 
         <div className="breezy-hollow-wrapper">
-          <div className="image-wrapper">
+          <div className="region image-wrapper">
             <img src='/images/homepage/breezy_hollow.png' />
           </div>
           <p>Breezy Hollow</p>
         </div>
 
         <div className="lost-lure-creek-wrapper">
-          <div className="image-wrapper">
+          <div className="region image-wrapper">
             <img src='/images/homepage/lost_lure_creek.png' />
           </div>
           <p>Lost Lure Creek</p>
         </div>
 
         <div className="saltwater-shores-wrapper">
-          <div className="image-wrapper">
+          <div className="region image-wrapper">
             <img src='/images/homepage/saltwater_shores.png' />
           </div>
           <p>Saltwater Shores</p>
         </div>
 
         <div className="sunburst-island-wrapper">
-          <div className="image-wrapper">
+          <div className="region image-wrapper">
             <img src='/images/homepage/sunburst_island.png' />
           </div>
           <p>Sunburst Island</p>
