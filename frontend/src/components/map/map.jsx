08dd29062a2c8b3b1e54a2f159782e79c0d58639
karleee <karleee@protonmail.com>
2020-04-07 13:59:41 -0700

    Fix searching

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -27,15 +27,50 @@ class Map extends Component {
       .then(options => {
         this.map = new google.maps.Map(map, options);
 
+        // before adding markers, set up bounds
+        let bounds = new google.maps.LatLngBounds();
+
         // Auto-centering the map
-        this.map.panTo(new google.maps.LatLng(options.center.lat, options.center.lng));
+        // this.map.panTo(new google.maps.LatLng(options.center.lat, options.center.lng));
+
+        // Displaying nearby spots
+        this.props.spots.forEach(spot => {
+          // Create a position from spot coordinates
+          const latitude = spot.latitude;
+          const longitude = spot.longitude;
+          const position = new google.maps.LatLng(latitude, longitude);
+
+          // Place markers on the map
+          const marker = new google.maps.Marker({
+            position,
+            map: this.map
+          });
+
+          // extend the bounds to fit this position
+          bounds.extend(position);
+        });
+
+        // Autozooming and autocentering if there are results
+        if (this.props.spots.length) {
+          // Autozooming
+          this.map.fitBounds(bounds);
+
+          // Adjust zooming value if too large
+          let theMap = this.map;
+          var listener = google.maps.event.addListener(theMap, "idle", function () {
+            if (theMap.getZoom() > 16) theMap.setZoom(16);
+            google.maps.event.removeListener(listener);
+          });
+
+          // Autocentering
+          this.map.panToBounds(bounds);
+        }
       });
   }
 
+  // Renders Map component
   render() {
-    return (
-      <div id="map-container"></div>
-    );
+    return <div id="map-container"></div>;
   }
 }
 
--- frontend/src/components/search/search.jsx
+++ frontend/src/components/search/search.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// import Map from '../map/map';
 import Map from '../map/map';
 import SearchResultsIndex from './search_results_index';
 import Footer from '../footer/footer';
@@ -22,11 +21,30 @@ class Search extends React.Component {
 	render() {
 		const { spots } = this.props;
 
+		// Getting lowercased location string from search input
+		const location = this.state.find_loc;
+		let locationArr;
+		let lowercasedLocation = [];
+		let foundSpots = [];
+		
+		if (location) {
+			locationArr = location.split(' ');
+			locationArr.forEach(word => lowercasedLocation.push(word.toLowerCase()));
+			lowercasedLocation = lowercasedLocation.join(' ');
+		}
+
+		// Searching for spots matching location
+		spots.forEach(spot => {
+			const city = spot.city.toLowerCase();
+			const state = spot.state.toLowerCase();
+			if (city === lowercasedLocation || state === lowercasedLocation) foundSpots.push(spot);
+		});
+
 		return (
 			<div className="search-container">
 				<div className="search-results-wrapper">
 				  <SearchResultsIndex spots={spots} />
-					<Map find_loc={this.state.find_loc} /> 
+					<Map find_loc={this.state.find_loc} spots={foundSpots} /> 
 				</div>
 
 				<Footer />
