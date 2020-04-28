7fc174794e319be1965dd6828ec3c3d8239c4516
karleee <karleee@protonmail.com>
2020-04-28 14:09:48 -0700

    Remove extra import from root reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -4,7 +4,6 @@ import session from './session_reducer';
 import entities from './entities_reducer';
 import ui from './ui_reducer';
 import currentSpot from './current_spot_reducer';
-import modal from './modal_reducer';
 import errors from './errors_reducer';
 
 // Root reducer
