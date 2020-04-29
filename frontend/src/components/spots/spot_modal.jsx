60deced8ff501f08a2c1ad0f0cae4c7dc39ca264
karleee <karleee@protonmail.com>
2020-04-28 21:46:28 -0700

    Fix session form overlay

--- frontend/src/components/spots/spot_modal.jsx
+++ frontend/src/components/spots/spot_modal.jsx
@@ -43,38 +43,32 @@ class SpotModal extends React.Component {
 
     return (
       <div className="spot modal-wrapper">
-        {/* <div className="spot content-wrapper"> */}
-          <div className="spot modal-main-image-wrapper">
-            {/* <div className="spot prev-arrow-wrapper" onClick={this.previous}>  */}
-            <i className="spot prev-arrow-icon-wrapper" onClick={this.previous}></i> 
-            {/* </div> */}
+        <div className="spot modal-main-image-wrapper">
+          <i className="spot prev-arrow-icon-wrapper" onClick={this.previous}></i> 
 
-            <div className="modal selected-image-wrapper">
-              <img src={spot.thumbnail_image_urls ? spot.thumbnail_image_urls[this.state.currentImage] : ''} alt="Spot photo" />
-            </div> 
+          <div className="modal selected-image-wrapper">
+            <img src={spot.thumbnail_image_urls ? spot.thumbnail_image_urls[this.state.currentImage] : ''} alt="Spot photo" />
+          </div> 
 
-            {/* <div className="modal next-arrow-wrapper" onClick={this.next}> */}
-            <i className="spot next-arrow-icon-wrapper" onClick={this.next}></i>
-            {/* </div> */}
+          <i className="spot next-arrow-icon-wrapper" onClick={this.next}></i>
+        </div>
+
+        <div className="spot side-images-wrapper">
+          <div className="spot close-wrapper" onClick={this.props.closeModal}>
+            <img src='/images/spot_modal/modal_close.png' />
           </div>
 
-          <div className="spot side-images-wrapper">
-            <div className="spot close-wrapper" onClick={this.props.closeModal}>
-              <img src='/images/spot_modal/modal_close.png' />
-            </div>
-
-            <div className="spot thumbnails-wrapper">
-              {spot.thumbnail_image_urls ? spot.thumbnail_image_urls.map((url, indx) =>
-                <div className={`spot image-wrapper ${indx === this.state.currentImage ? "active" : ""}`} onClick={() => this.handleClick(indx)}>
-                  <img src={url} alt="Spot photo" />
-                </div>) : ''}
-            </div>
-
-            <div className="spot numbering-wrapper">
-              <p>{this.state.currentImage + 1} / {spot.thumbnail_image_urls.length}</p>
-            </div>
+          <div className="spot thumbnails-wrapper">
+            {spot.thumbnail_image_urls ? spot.thumbnail_image_urls.map((url, indx) =>
+              <div className={`spot image-wrapper ${indx === this.state.currentImage ? "active" : ""}`} onClick={() => this.handleClick(indx)}>
+                <img src={url} alt="Spot photo" />
+              </div>) : ''}
           </div>
-        {/* </div> */}
+
+          <div className="spot numbering-wrapper">
+            <p>{this.state.currentImage + 1} / {spot.thumbnail_image_urls.length}</p>
+          </div>
+        </div>
       </div>
     );
   }
