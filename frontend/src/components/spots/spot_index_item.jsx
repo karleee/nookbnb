1691a4b47cd506185488d3a5cfe237d39b385b69
karleee <karleee@protonmail.com>
2020-04-30 15:57:02 -0700

    Remove extra div

--- frontend/src/components/spots/spot_index_item.jsx
+++ frontend/src/components/spots/spot_index_item.jsx
@@ -7,20 +7,18 @@ import '../../assets/stylesheets/spots/spot_index.css';
 const SpotIndexItem = ({ spot }) => (
   <li>
     <Link to={`/spots/${spot._id}`}>
-      <div className="top-spot-wrapper">
-        <div className="top-rated image-wrapper">
-          <img src={spot.thumbnail_image_urls ? spot.thumbnail_image_urls[0] : ''} alt="Spot thumbnail" />
+      <div className="top-rated image-wrapper">
+        <img src={spot.thumbnail_image_urls ? spot.thumbnail_image_urls[0] : ''} alt="Spot thumbnail" />
+      </div>
+
+      <div className="top-rated-text-wrapper">
+        <div className="superhost-wrapper">
+          <p>Superhost</p>
+          <p>{spot.country}</p>
         </div>
 
-        <div className="top-rated-text-wrapper">
-          <div className="superhost-wrapper">
-            <p>Superhost</p>
-            <p>{spot.country}</p>
-          </div>
-
-          <h4>{spot.name.length > 26 ? spot.name.substr(0, 25) + '...' : spot.name}</h4> 
-          <p><span className="price-wrapper">${spot.price}</span> / night</p>
-        </div>
+        <h4>{spot.name.length > 26 ? spot.name.substr(0, 25) + '...' : spot.name}</h4> 
+        <p><span className="price-wrapper">${spot.price}</span> / night</p>
       </div>
     </Link>
   </li>
