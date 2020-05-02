e7cb25be55def1c5e6ca821425815ac28b04a744
karleee <karleee@protonmail.com>
2020-05-01 20:59:18 -0700

    Fix price dynamic styling

--- frontend/src/components/spots/spot_detail.jsx
+++ frontend/src/components/spots/spot_detail.jsx
@@ -73,7 +73,7 @@ class SpotDetail extends React.Component {
                 <h1>{spot.name}</h1>
                 <p>{spot.city}</p>
               </div>
-              
+
               <img src={user.profile_image_url} alt="Profile image" /> 
             </div>
 
@@ -82,159 +82,155 @@ class SpotDetail extends React.Component {
             </div>
           </div>
 
-          {/* <div className="spot-index-item-detail-description"> */}
-            <div className="spot-detail body-wrapper">
-              {/* <div className="description-wrapper"> */}
-                <div className="spot-detail gen-info-wrapper">
-                  <p>{spot.occupancy} {spot.occupancy > 1 ? 'guests' : 'guest'}</p>
-                  <p>{spot.bedrooms} {spot.bedrooms > 1 ? 'bedrooms' : 'bedroom'}</p>
-                  <p>{spot.beds} {spot.beds > 1 ? 'beds' : 'bed'}</p>
-                  <p>{spot.baths} {spot.baths > 1 ? 'baths' : 'bath'}</p>
+          <div className="spot-detail body-wrapper">
+            <div className="spot-detail gen-info-wrapper">
+              <p>{spot.occupancy} {spot.occupancy > 1 ? 'guests' : 'guest'}</p>
+              <p>{spot.bedrooms} {spot.bedrooms > 1 ? 'bedrooms' : 'bedroom'}</p>
+              <p>{spot.beds} {spot.beds > 1 ? 'beds' : 'bed'}</p>
+              <p>{spot.baths} {spot.baths > 1 ? 'baths' : 'bath'}</p>
+            </div>
+
+            <div className="spot-detail perks-wrapper">
+              <div className="entire-home-perk-wrapper">
+                <div className="image-wrapper">
+                  <i className="star-icon"><img src='/images/spot_detail/star_icon.png' /></i>
                 </div>
 
-                <div className="spot-detail perks-wrapper">
-                  <div className="entire-home-perk-wrapper">
-                    <div className="image-wrapper">
-                      <i className="star-icon"><img src='/images/spot_detail/star_icon.png' /></i>
-                    </div>
+                <div className="perk-wrapper">
+                  <h3>Entire home</h3>
+                  <p>You'll have the house to yourself.</p>
+                </div>
+              </div>
 
-                    <div className="perk-wrapper">
-                      <h3>Entire home</h3>
-                      <p>You'll have the house to yourself.</p>
-                    </div>
-                  </div>
-
-                  <div className="sparkling-clean-perk-wrapper">
-                    <div className="image-wrapper">
-                      <i className="cleaner-icon"><img src='/images/spot_detail/bottle_icon.png' /></i>
-                    </div>
-
-                    <div className="perk-wrapper">
-                      <h3>Sparkling clean</h3>
-                      <p>Recent guests said this place was sparkling clean.</p>
-                    </div>
-                  </div>
-
-                  <div className="superhost-perk-wrapper"> 
-                    <div className="image-wrapper">
-                      <i className="badge-icon"><img src='/images/spot_detail/badge_icon.png' /></i>
-                    </div>
-
-                    <div className="perk-wrapper">
-                      <h3>{usernameCapitalized} is a Superhost</h3>
-                      <p>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
-                    </div>
-                  </div>
-
-                  <div className="check-in-perk-wrapper">
-                    <div className="image-wrapper">
-                      <i className="house-icon"><img src='/images/spot_detail/check_in_icon.png' /></i>
-                    </div>
-
-                    <div className="perk-wrapper">
-                      <h3>Great check-in experience</h3>
-                      <p>Recent guests gave the check-in process a 5-star rating.</p>
-                    </div>
-                  </div>
+              <div className="sparkling-clean-perk-wrapper">
+                <div className="image-wrapper">
+                  <i className="cleaner-icon"><img src='/images/spot_detail/bottle_icon.png' /></i>
                 </div>
 
-                <div className="spot-detail description-wrapper">
-                  <p>{spot.description}</p>
+                <div className="perk-wrapper">
+                  <h3>Sparkling clean</h3>
+                  <p>Recent guests said this place was sparkling clean.</p>
+                </div>
+              </div>
+
+              <div className="superhost-perk-wrapper"> 
+                <div className="image-wrapper">
+                  <i className="badge-icon"><img src='/images/spot_detail/badge_icon.png' /></i>
                 </div>
 
-                <div className="spot-detail sleeping-wrapper">
-                  <h2>Sleeping arrangements</h2>
-                  <SleepingArrangementIndex bedrooms={spot.bedrooms} />
-                </div> 
-
-                <div className="spot-detail amenities-wrapper">
-                  <h2>Amenities</h2>
-                  
-                  <div className="amenities-grid">
-                    {spot.amenities && spot.amenities[0].wifi ? 
-                      <div className="wifi-wrapper">
-                        <div className="image-wrapper">
-                          <i className="wifi-icon"><img src='/images/spot_detail/wifi_icon.png' /></i>
-                        </div>
-                        
-                        <div className="label-wrapper">
-                          <p>Wifi</p> 
-                        </div>
-                      </div> : ""
-                    }
-
-                    {spot.amenities && spot.amenities[0].kitchen ?
-                      <div className="kitchen-wrapper">
-                        <div className="image-wrapper">
-                          <i className="kitchen-icon"><img src='/images/spot_detail/kitchen_icon.png' /></i>
-                        </div>
-
-                        <div className="label-wrapper">
-                          <p>Kitchen</p>
-                        </div>
-                      </div> : ""
-                    }
-
-                    {spot.amenities && spot.amenities[0].breakfast ?
-                      <div className="breakfast-wrapper">
-                        <div className="image-wrapper">
-                          <i className="breakfast-icon"><img src='/images/spot_detail/breakfast_icon.png' /></i>
-                        </div>
-
-                        <div className="label-wrapper">
-                          <p>Breakfast</p>
-                        </div>
-                      </div> : ""
-                    }
-
-                    {spot.amenities && spot.amenities[0].parking ?
-                      <div className="parking-wrapper">
-                        <div className="image-wrapper">
-                          <i className="parking-icon"><img src='/images/spot_detail/parking_icon.png' /></i>
-                        </div>
-
-                        <div className="label-wrapper">
-                          <p>Free parking on premises</p>
-                        </div>
-                      </div> : ""
-                    }
-
-                    {spot.amenities && spot.amenities[0].pool ?
-                      <div className="pool-wrapper">
-                        <div className="image-wrapper">
-                          <i className="pool-icon"><img src='/images/spot_detail/pool_icon.png' /></i>
-                        </div>
-
-                        <div className="label-wrapper">
-                          <p>Pool</p>
-                        </div>
-                      </div> : ""
-                    }
-
-                    {spot.amenities && spot.amenities[0].essentials ?
-                      <div className="essentials-wrapper">
-                        <div className="image-wrapper">
-                          <i className="essentials-icon"><img src='/images/spot_detail/essentials_icon.png' /></i>
-                        </div>
-
-                        <div className="label-wrapper">
-                          <p>Essentials</p>
-                        </div>
-                      </div> : ""
-                    }
-                  </div>
+                <div className="perk-wrapper">
+                  <h3>{usernameCapitalized} is a Superhost</h3>
+                  <p>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
                 </div>
-              {/* </div> */}
+              </div>
+
+              <div className="check-in-perk-wrapper">
+                <div className="image-wrapper">
+                  <i className="house-icon"><img src='/images/spot_detail/check_in_icon.png' /></i>
+                </div>
+
+                <div className="perk-wrapper">
+                  <h3>Great check-in experience</h3>
+                  <p>Recent guests gave the check-in process a 5-star rating.</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="spot-detail description-wrapper">
+              <p>{spot.description}</p>
+            </div>
+
+            <div className="spot-detail sleeping-wrapper">
+              <h2>Sleeping arrangements</h2>
+              <SleepingArrangementIndex bedrooms={spot.bedrooms} />
             </div> 
-          {/* </div> */}
 
-          <div className="spot-index-detail-booking-form">
+            <div className="spot-detail amenities-wrapper">
+              <h2>Amenities</h2>
+              
+              <div className="amenities-grid">
+                {spot.amenities && spot.amenities[0].wifi ? 
+                  <div className="wifi-wrapper">
+                    <div className="image-wrapper">
+                      <i className="wifi-icon"><img src='/images/spot_detail/wifi_icon.png' /></i>
+                    </div>
+                    
+                    <div className="label-wrapper">
+                      <p>Wifi</p> 
+                    </div>
+                  </div> : ""
+                }
+
+                {spot.amenities && spot.amenities[0].kitchen ?
+                  <div className="kitchen-wrapper">
+                    <div className="image-wrapper">
+                      <i className="kitchen-icon"><img src='/images/spot_detail/kitchen_icon.png' /></i>
+                    </div>
+
+                    <div className="label-wrapper">
+                      <p>Kitchen</p>
+                    </div>
+                  </div> : ""
+                }
+
+                {spot.amenities && spot.amenities[0].breakfast ?
+                  <div className="breakfast-wrapper">
+                    <div className="image-wrapper">
+                      <i className="breakfast-icon"><img src='/images/spot_detail/breakfast_icon.png' /></i>
+                    </div>
+
+                    <div className="label-wrapper">
+                      <p>Breakfast</p>
+                    </div>
+                  </div> : ""
+                }
+
+                {spot.amenities && spot.amenities[0].parking ?
+                  <div className="parking-wrapper">
+                    <div className="image-wrapper">
+                      <i className="parking-icon"><img src='/images/spot_detail/parking_icon.png' /></i>
+                    </div>
+
+                    <div className="label-wrapper">
+                      <p>Free parking on premises</p>
+                    </div>
+                  </div> : ""
+                }
+
+                {spot.amenities && spot.amenities[0].pool ?
+                  <div className="pool-wrapper">
+                    <div className="image-wrapper">
+                      <i className="pool-icon"><img src='/images/spot_detail/pool_icon.png' /></i>
+                    </div>
+
+                    <div className="label-wrapper">
+                      <p>Pool</p>
+                    </div>
+                  </div> : ""
+                }
+
+                {spot.amenities && spot.amenities[0].essentials ?
+                  <div className="essentials-wrapper">
+                    <div className="image-wrapper">
+                      <i className="essentials-icon"><img src='/images/spot_detail/essentials_icon.png' /></i>
+                    </div>
+
+                    <div className="label-wrapper">
+                      <p>Essentials</p>
+                    </div>
+                  </div> : ""
+                }
+              </div>
+            </div>
+          </div> 
+
+          <div className="spot-detail booking-wrapper">
             <h2>Select dates</h2>
             <Datepicker />
           </div>
         </div>
 
-        <Footer />
+        {/* <Footer /> */}
       </div> 
     );
   }
